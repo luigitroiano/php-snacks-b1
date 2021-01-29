aa390a9c0213b778ee25a514c142a2d12f6391df
LuigiTroiano <troiano.luigi8@gmail.com>
2021-01-29 18:41:22 +0100

    Snack 1 - Completato
    Dati inseriti con ciclo for

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ while (count($homeTeams) < 15) {
   }
 };
 
-var_dump($homeTeams);
+#var_dump($homeTeams);
 //differenza tra array(teamsNBA) e array(homeTeams), mi da come risultato le altre 15 squadre non uscite dalla pesca randomica
 $guestTeams = array_diff($teamsNBA, $homeTeams);
 //metto in ordine le key degli elementi dell'array (guestTeams), perchè dall'operazione precedente mi sono ritrovato con delle key non in ordine e non partivano da zero
@@ -40,97 +40,127 @@ $orderedGuestTeams = [];
 foreach ($guestTeams as $value) {
   array_push($orderedGuestTeams, $value);
 }
-var_dump($orderedGuestTeams);
+#var_dump($orderedGuestTeams);
 $matches = [
-  "Match 1" => [
-    "Home" => $homeTeams[0],
-    "Guest" => $orderedGuestTeams[0],
-    "Home Point" => $randomScore[0],
-    "Guest Point" => $randomScore[1],
+  [
+    "Match" => [
+      "Home" => $homeTeams[0],
+      "Guest" => $orderedGuestTeams[0],
+      "Home Point" => $randomScore[0],
+      "Guest Point" => $randomScore[1],
+    ],
   ],
-  "Match 2" => [
-    "Home" => $homeTeams[1],
-    "Guest" => $orderedGuestTeams[1],
-    "Home Point" => $randomScore[2],
-    "Guest Point" => $randomScore[3],
+  [
+    "Match" => [
+      "Home" => $homeTeams[1],
+      "Guest" => $orderedGuestTeams[1],
+      "Home Point" => $randomScore[2],
+      "Guest Point" => $randomScore[3],
+    ],
   ],
-  "Match 3" => [
-    "Home" => $homeTeams[2],
-    "Guest" => $orderedGuestTeams[2],
-    "Home Point" => $randomScore[4],
-    "Guest Point" => $randomScore[5],
+  [
+    "Match" => [
+      "Home" => $homeTeams[2],
+      "Guest" => $orderedGuestTeams[2],
+      "Home Point" => $randomScore[4],
+      "Guest Point" => $randomScore[5],
+    ],
   ],
-  "Match 4" => [
-    "Home" => $homeTeams[3],
-    "Guest" => $orderedGuestTeams[3],
-    "Home Point" => $randomScore[6],
-    "Guest Point" => $randomScore[7],
+  [
+    "Match" => [
+      "Home" => $homeTeams[3],
+      "Guest" => $orderedGuestTeams[3],
+      "Home Point" => $randomScore[6],
+      "Guest Point" => $randomScore[7],
+    ],
   ],
-  "Match 5" => [
-    "Home" => $homeTeams[4],
-    "Guest" => $orderedGuestTeams[4],
-    "Home Point" => $randomScore[8],
-    "Guest Point" => $randomScore[9],
+  [
+    "Match" => [
+      "Home" => $homeTeams[4],
+      "Guest" => $orderedGuestTeams[4],
+      "Home Point" => $randomScore[8],
+      "Guest Point" => $randomScore[9],
+    ],
   ],
-  "Match 6" => [
-    "Home" => $homeTeams[5],
-    "Guest" => $orderedGuestTeams[5],
-    "Home Point" => $randomScore[10],
-    "Guest Point" => $randomScore[11],
+  [
+    "Match" => [
+      "Home" => $homeTeams[5],
+      "Guest" => $orderedGuestTeams[5],
+      "Home Point" => $randomScore[10],
+      "Guest Point" => $randomScore[11],
+    ],
   ],
-  "Match 7" => [
-    "Home" => $homeTeams[6],
-    "Guest" => $orderedGuestTeams[6],
-    "Home Point" => $randomScore[12],
-    "Guest Point" => $randomScore[13],
+  [
+    "Match" => [
+      "Home" => $homeTeams[6],
+      "Guest" => $orderedGuestTeams[6],
+      "Home Point" => $randomScore[12],
+      "Guest Point" => $randomScore[13],
+    ],
   ],
-  "Match 8" => [
-    "Home" => $homeTeams[7],
-    "Guest" => $orderedGuestTeams[7],
-    "Home Point" => $randomScore[14],
-    "Guest Point" => $randomScore[15],
+  [
+    "Match" => [
+      "Home" => $homeTeams[7],
+      "Guest" => $orderedGuestTeams[7],
+      "Home Point" => $randomScore[14],
+      "Guest Point" => $randomScore[15],
+    ],
   ],
-  "Match 9" => [
-    "Home" => $homeTeams[8],
-    "Guest" => $orderedGuestTeams[8],
-    "Home Point" => $randomScore[16],
-    "Guest Point" => $randomScore[17],
+  [
+    "Match" => [
+      "Home" => $homeTeams[8],
+      "Guest" => $orderedGuestTeams[8],
+      "Home Point" => $randomScore[16],
+      "Guest Point" => $randomScore[17],
+    ],
   ],
-  "Match 10" => [
-    "Home" => $homeTeams[9],
-    "Guest" => $orderedGuestTeams[9],
-    "Home Point" => $randomScore[18],
-    "Guest Point" => $randomScore[19],
+  [
+    "Match" => [
+      "Home" => $homeTeams[9],
+      "Guest" => $orderedGuestTeams[9],
+      "Home Point" => $randomScore[18],
+      "Guest Point" => $randomScore[19],
+    ],
   ],
-  "Match 11" => [
-    "Home" => $homeTeams[10],
-    "Guest" => $orderedGuestTeams[10],
-    "Home Point" => $randomScore[20],
-    "Guest Point" => $randomScore[21],
+  [
+    "Match" => [
+      "Home" => $homeTeams[10],
+      "Guest" => $orderedGuestTeams[10],
+      "Home Point" => $randomScore[20],
+      "Guest Point" => $randomScore[21],
+    ],
   ],
-  "Match 12" => [
-    "Home" => $homeTeams[11],
-    "Guest" => $orderedGuestTeams[11],
-    "Home Point" => $randomScore[22],
-    "Guest Point" => $randomScore[23],
+  [
+    "Match" => [
+      "Home" => $homeTeams[11],
+      "Guest" => $orderedGuestTeams[11],
+      "Home Point" => $randomScore[22],
+      "Guest Point" => $randomScore[23],
+    ],
   ],
-  "Match 13" => [
-    "Home" => $homeTeams[12],
-    "Guest" => $orderedGuestTeams[12],
-    "Home Point" => $randomScore[24],
-    "Guest Point" => $randomScore[25],
+  [
+    "Match" => [
+      "Home" => $homeTeams[12],
+      "Guest" => $orderedGuestTeams[12],
+      "Home Point" => $randomScore[24],
+      "Guest Point" => $randomScore[25],
+    ],
   ],
-  "Match 14" => [
-    "Home" => $homeTeams[13],
-    "Guest" => $orderedGuestTeams[13],
-    "Home Point" => $randomScore[26],
-    "Guest Point" => $randomScore[27],
+  [
+    "Match" => [
+      "Home" => $homeTeams[13],
+      "Guest" => $orderedGuestTeams[13],
+      "Home Point" => $randomScore[26],
+      "Guest Point" => $randomScore[27],
+    ],
   ],
-  "Match 15" => [
-    "Home" => $homeTeams[14],
-    "Guest" => $orderedGuestTeams[14],
-    "Home Point" => $randomScore[28],
-    "Guest Point" => $randomScore[29],
+  [
+    "Match" => [
+      "Home" => $homeTeams[14],
+      "Guest" => $orderedGuestTeams[14],
+      "Home Point" => $randomScore[28],
+      "Guest Point" => $randomScore[29],
+    ],
   ],
 ];
 
@@ -159,9 +189,15 @@ $matches = [
     Stampiamo a schermo tutte le partite con questo schema:
     Olimpia Milano - Cantù | 55 - 60 -->
 
-
-
-
+  <h1>Campionato NBA <em>- 1º Giornata</em>
+  </h1>
+  <ul>
+    <?php
+    for ($i = 0; $i < count($matches); ++$i) {
+      echo "<li>" . $matches[$i]["Match"]["Home"] . " - " . $matches[$i]["Match"]["Guest"] . " (" . $matches[$i]["Match"]["Home Point"] . " - " . $matches[$i]["Match"]["Guest Point"] . ") " . "</li>";
+    };
+    ?>
+  </ul>
   <!-- js -->
   <script src="./dist/js/app.js"></script>
 </body>
