a4bd9bf3b4e3ab1e64016ff9d805e9abacff78a4
LuigiTroiano <troiano.luigi8@gmail.com>
2021-01-29 18:47:52 +0100

    Initial commit Snack 2

--- Snack 1/index.php
+++ Snack 1/index.php
@@ -177,7 +177,7 @@ $matches = [
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PHP Snack</title>
+  <title>PHP Snack 1</title>
   <!-- css -->
   <link rel="stylesheet" href="./dist/css/app.css">
 </head>
