de09fb13375e10ab7411e8b9700d2b948b2f88cf
LuigiTroiano <troiano.luigi8@gmail.com>
2021-01-29 13:23:19 +0100

    Initial commit 2.0

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Snack</title>
+  <!-- css -->
+  <link rel="stylesheet" href="./dist/css/app.css">
+</head>
+<body>
+  
+
+  <!-- js -->
+  <script src="./dist/js/app.js"></script>
+</body>
+</html>
